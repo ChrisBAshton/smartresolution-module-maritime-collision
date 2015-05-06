dccd82da6a4e988199c3aff96a415d33c0fdefd0
ChrisBAshton <chrisashtonweb@gmail.com>
2015-05-06 12:27:56 +0100

    fix references

--- MaritimeCollision.php
+++ MaritimeCollision.php
@@ -1,34 +1,18 @@
 <?php
 
+/**
+ * Defines the methods which handle maritime-collision dispute HTTP methods.
+ */
 class MaritimeCollision {
 
     private $firstAgent   = 0;
     private $secondAgent  = 0;
     private $resultsReady = false;
 
-    public function showMaritimeCollisionOption() {
-        dashboard_add_item(array(
-            'title' => 'Maritime Collision',
-            'image' => get_module_url() . '/assets/anchor.png',
-            'href'  => get_dispute_url() . '/maritime-collision'
-        ), true);
-    }
-
-    public function showMaritimeCollisionMetaOptions() {
-        dashboard_add_items(array(
-            array(
-                'title' => 'Search historical maritime disputes',
-                'image' => get_module_url() . '/assets/magnifying_glass.png',
-                'href'  => '/maritime-collision-search'
-            ),
-            array(
-                'title' => 'About Maritime Collision',
-                'image' => get_module_url() . '/assets/about.png',
-                'href'  => '/about-maritime-collision'
-            )
-        ));
-    }
-
+    /**
+     * Handles the HTTP GET '/maritime-collision', rendering either a disclaimer page, questions form
+     * or results page depending on the history of the dispute.
+     */
     public function maritimeCollisionIndex() {
         $this->checkMaritimeStatus();
 
@@ -58,6 +42,116 @@ class MaritimeCollision {
         }
     }
 
+    /**
+     * Checks if both agents have agreed to the disclaimer and also checks if results are ready, setting
+     * the MaritimeCollision class private attributes.
+     * This function should be called before rendering Maritime Collision dispute pages.
+     * It was tempting to put this in a constructor but that causes problems when calling dispute-independent
+     * pages, such as "About Maritime Collision".
+     */
+    private function checkMaritimeStatus() {
+        if (get('setup.id_of_first_agent') === false) {
+            // record was not found - this is the first time the module has been loaded in the dispute.
+            // We need to create a record. No need to manually specify any property values, since we've
+            // defined some defaults in the table setup.
+            createRow('setup');
+        }
+
+        // these don't necessarily correspond to Agent A or Agent B in the SmartResolution core software,
+        // we just need to identify the two agents and keep them separate. The ordering doesn't matter.
+        $this->firstAgent  = (int) get('setup.id_of_first_agent');
+        $this->secondAgent = (int) get('setup.id_of_second_agent');
+        $this->checkIfResultsAreReady();
+    }
+
+    /**
+     * Denotes whether or not the module has calculated the results of the dispute or if it still needs to ask either agent any more questions.
+     * @return boolean True if results are ready, false if not.
+     */
+    private function checkIfResultsAreReady() {
+        $this->resultsReady = (
+            $this->bothAgentsInitiated() &&
+            $this->agentHasAnsweredAllQuestions($this->firstAgent) &&
+            $this->agentHasAnsweredAllQuestions($this->secondAgent)
+        );
+    }
+
+    /**
+     * Denotes whether or not both agents have initiated the module (accepted the disclaimer).
+     * @return boolean True if both agents have initiated, otherwise false.
+     */
+    private function bothAgentsInitiated() {
+        return $this->firstAgent > 0 && $this->secondAgent > 0;
+    }
+
+    /**
+     * Denotes whether or not the given agent has answered all of the relevant questions or if there are more questions to answer.
+     * @param  int $agentID Login ID of the agent.
+     * @return boolean      True if agent has answered all questions, otherwise false.
+     */
+    private function agentHasAnsweredAllQuestions($agentID) {
+        return (0 === count($this->getQuestionsForAgent($agentID)));
+    }
+
+    /**
+     * Returns an array of unanswered questions for the given agent. If the agent has already answered all questions, returns an empty array.
+     * @param  int $agentID Login ID of the agent.
+     * @return array        Array of questions, each element represented by an associative array.
+     */
+    private function getQuestionsForAgent($agentID) {
+        $string       = file_get_contents(__DIR__ . '/questions.json');
+        $allQuestions = json_decode($string, true);
+        $questions    = array();
+
+        foreach($allQuestions as $question) {
+            $alreadyAnswered = get('answers.answer', array(
+                'agent_id' => $agentID,
+                'question' => $question['id']
+            ));
+
+            if (!$alreadyAnswered && $this->satisfiesPrerequisites($question, $agentID)) {
+                array_push($questions, $question);
+            }
+        }
+
+        return $questions;
+    }
+
+    /**
+     * Denotes whether or not the given question has all of its prerequisites satisfied. For example, for question 2 to be displayed it may require that question 1 was answered "no". The prerequisites are defined in the JSON in the following format:
+     *
+     *  "prerequisites": [
+     *      {
+     *          "question_id":     "question_1",
+     *          "required_answer": "no"
+     *      }
+     *  ]
+     *
+     *  The Agent ID is required because the function checks the answers of that agent to see if the prerequisites are satisfied.
+     *
+     * @param  array $question  Associative array representing the question.
+     * @param  int   $agentID   ID of the agent we want to check has fulfilled the prerequisites.
+     * @return boolean          True if satisfied, false if not.
+     */
+    private function satisfiesPrerequisites($question, $agentID) {
+        if (isset($question['prerequisites'])) {
+            foreach($question['prerequisites'] as $prereq) {
+                $prereqAnswer = get('answers.answer', array(
+                    'agent_id' => $agentID,
+                    'question' => $prereq['question_id'],
+                    'answer'   => $prereq['required_answer']
+                ));
+                if (!$prereqAnswer) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Renders the questions form.
+     */
     public function renderQuestions() {
         $questions = $this->getQuestionsForAgent(get_login_id());
 
@@ -77,6 +171,38 @@ class MaritimeCollision {
         }
     }
 
+    /**
+     * Adds the maritime collision option to the dispute dashboard.
+     */
+    public function showMaritimeCollisionOption() {
+        dashboard_add_item(array(
+            'title' => 'Maritime Collision',
+            'image' => get_module_url() . '/assets/anchor.png',
+            'href'  => get_dispute_url() . '/maritime-collision'
+        ), true);
+    }
+
+    /**
+     * Adds various maritime collision options to the homescreen dashboard, including an 'About' and a 'Search' page.
+     */
+    public function showMaritimeCollisionMetaOptions() {
+        dashboard_add_items(array(
+            array(
+                'title' => 'Search historical maritime disputes',
+                'image' => get_module_url() . '/assets/magnifying_glass.png',
+                'href'  => '/maritime-collision-search'
+            ),
+            array(
+                'title' => 'About Maritime Collision',
+                'image' => get_module_url() . '/assets/about.png',
+                'href'  => '/about-maritime-collision'
+            )
+        ));
+    }
+
+    /**
+     * POST method; the logged in agent accepts the disclaimer, initiating the module.
+     */
     public function initiateMaritimeCollision() {
         $loggedInAgent = get_login_id();
 
@@ -101,6 +227,10 @@ class MaritimeCollision {
         header('Location: ' . get_dispute_url() . '/maritime-collision');
     }
 
+    /**
+     * POST method; submits an agent's answer or answers to the database. Then redirects to maritime collision landing page.
+     * @param  F3 $f3 The global F3 object containing the POSTed answers.
+     */
     public function answerQuestion($f3) {
         foreach($f3->get('POST') as $questionID => $value) {
             createRow('answers', array(
@@ -117,76 +247,5 @@ class MaritimeCollision {
         );
 
         header('Location: ' . get_dispute_url() . '/maritime-collision');
-    }
-
-    /**
-     * checkMaritimeStatus() should be called before rendering Maritime Collision dispute pages.
-     * It was tempting to put this in a constructor but that causes problems when calling dispute-independent
-     * pages, such as "About Maritime Collision".
-     */
-    private function checkMaritimeStatus() {
-        if (get('setup.id_of_first_agent') === false) {
-            // record was not found - this is the first time the module has been loaded in the dispute.
-            // We need to create a record. No need to manually specify any property values, since we've
-            // defined some defaults in the table setup.
-            createRow('setup');
-        }
-
-        // these don't necessarily correspond to Agent A or Agent B in the SmartResolution core software,
-        // we just need to identify the two agents and keep them separate. The ordering doesn't matter.
-        $this->firstAgent  = (int) get('setup.id_of_first_agent');
-        $this->secondAgent = (int) get('setup.id_of_second_agent');
-        $this->checkIfResultsAreReady();
-    }
-
-    private function bothAgentsInitiated() {
-        return $this->firstAgent > 0 && $this->secondAgent > 0;
-    }
-
-    private function checkIfResultsAreReady() {
-        $this->resultsReady = (
-            $this->bothAgentsInitiated() &&
-            $this->agentHasAnsweredAllQuestions($this->firstAgent) &&
-            $this->agentHasAnsweredAllQuestions($this->secondAgent)
-        );
-    }
-
-    private function agentHasAnsweredAllQuestions($agentID) {
-        return (0 === count($this->getQuestionsForAgent($agentID)));
-    }
-
-    private function getQuestionsForAgent($agentID) {
-        $string       = file_get_contents(__DIR__ . '/questions.json');
-        $allQuestions = json_decode($string, true);
-        $questions    = array();
-
-        foreach($allQuestions as $question) {
-            $alreadyAnswered = get('answers.answer', array(
-                'agent_id' => $agentID,
-                'question' => $question['id']
-            ));
-
-            if (!$alreadyAnswered && $this->satisfiesPrerequisites($question, $agentID)) {
-                array_push($questions, $question);
-            }
-        }
-
-        return $questions;
-    }
-
-    private function satisfiesPrerequisites($question, $agentID) {
-        if (isset($question['prerequisites'])) {
-            foreach($question['prerequisites'] as $prereq) {
-                $prereqAnswer = get('answers.answer', array(
-                    'agent_id' => $agentID,
-                    'question' => $prereq['question_id'],
-                    'answer'   => $prereq['required_answer']
-                ));
-                if (!$prereqAnswer) {
-                    return false;
-                }
-            }
-        }
-        return true;
     }
 }
--- ResultsCalculator.php
+++ ResultsCalculator.php
@@ -1,39 +1,31 @@
 <?php
 
+/**
+ * Responsible for calculating the maritime collision law result according to the agents' answers.
+ */
 class ResultsCalculator {
 
     private $questions    = array();
-    private $yourAnswers  = array();
-    private $theirAnswers = array();
+    private $yourAnswers  = array(); // answers of the logged in agent
+    private $theirAnswers = array(); // answers of the other agent in the dispute
 
+    /**
+     * Constructor: gathers the necessary data, calculates a result and renders the result in HTML.
+     */
     function __construct() {
         $this->setUp();
-
-        $results = array();
-        foreach($this->questions as $question) {
-            $result = array(
-                'question_id'  => $question['id'],
-                'question'     => $question['text'],
-                'your_answer'  => $this->getAnswerToQuestion($question['id'], $this->yourAnswers),
-                'their_answer' => $this->getAnswerToQuestion($question['id'], $this->theirAnswers)
-            );
-            $this->checkIfAnswersTally($result);
-
-            if ($result['your_answer'] === 'N/A' && $result['their_answer'] === 'N/A') {
-                // the question was not applicable for either agent, and therefore is not relevant
-                // for the purposes of our results and should not be added to the array.
-            }
-            else {
-                array_push($results, $result);
-            }
-        }
+        $answers   = $this->getAnswers();
+        $summaries = $this->tryToDeduceSummary($answers);
 
         render(get_module_url() . '/views/results.html', array(
-            'results'   => $results,
-            'summaries' => $this->tryToDeduceSummary($results)
+            'results'   => $answers,
+            'summaries' => $summaries
         ));
     }
 
+    /**
+     * Retrieves the questions from the JSON and the agents' answers from the database, storing the results in private attributes to make available to the rest of the class.
+     */
     private function setUp() {
         $this->questions = json_decode(file_get_contents(__DIR__ . '/questions.json'), true);
         $answers = get_multiple('answers.*');
@@ -48,6 +40,44 @@ class ResultsCalculator {
         }
     }
 
+    /**
+     * Retrieves an array containing questions, answers and whether or not the answers tally.
+     * @return array
+     */
+    private function getAnswers() {
+        $answers = array();
+        foreach($this->questions as $question) {
+            $answer = array(
+                'question_id'  => $question['id'],
+                'question'     => $question['text'],
+                'your_answer'  => $this->getAnswerToQuestion($question['id'], $this->yourAnswers),
+                'their_answer' => $this->getAnswerToQuestion($question['id'], $this->theirAnswers)
+            );
+
+            if ($this->answersTally($answer)) {
+                $answer['tally'] = 'YES';
+            }
+            else {
+                $answer['tally'] = 'NO';
+            }
+
+            if ($answer['your_answer'] === 'N/A' && $answer['their_answer'] === 'N/A') {
+                // the question was not applicable for either agent, and therefore is not relevant
+                // for the purposes of our results and should not be added to the array.
+            }
+            else {
+                array_push($answers, $answer);
+            }
+        }
+        return $answers;
+    }
+
+    /**
+     * Retrieves from the collection of answers the answer which corresponds to the given question ID.
+     * @param  string $questionID Unique ID of the question.
+     * @param  array  $answers    Array of answers.
+     * @return string             Answer to the question.
+     */
     private function getAnswerToQuestion($questionID, $answers) {
         foreach($answers as $answer) {
             if ($questionID === $answer['question']) {
@@ -57,34 +87,8 @@ class ResultsCalculator {
         return 'N/A';
     }
 
-    private function checkIfAnswersTally(&$result) {
-        if ($this->answersTally($result)) {
-            $result['tally'] = 'YES';
-        }
-        else {
-            $result['tally'] = 'NO';
-        }
-    }
 
-    private function answerIsOneOf($result, $possibleAnswers) {
-        foreach($possibleAnswers as $answer) {
-            if ($this->answerIs($result, $answer)) {
-                return true;
-            }
-        }
-        return false;
-    }
 
-    private function answersMatch($result) {
-        return $result['your_answer'] === $result['their_answer'];
-    }
-
-    private function answerIs($result, $answer) {
-        return (
-            ($result['your_answer'] === $answer[0] && $result['their_answer'] === $answer[1]) ||
-            ($result['your_answer'] === $answer[1] && $result['their_answer'] === $answer[0])
-        );
-    }
 
     private function tryToDeduceSummary($results) {
         $canDeduceSummary = true;
@@ -114,33 +118,78 @@ class ResultsCalculator {
         throw new Exception('Could not find the question.');
     }
 
-    // ************************************************************************ the functions below are tightly coupled with the specific questions in questions.json. Everything above this line is more generic and could be reused in other modules.
-
-    private function answersTally($result) {
-        switch($result['question_id']) {
+    /**
+     * Deduces whether or not the agents' answers tally. This isn't just a case of checking if the answers are the same: some answers need to be different for the overall picture to match, e.g. "Which vessel was damaged?" => ("mine" and "theirs") or ("theirs" and "mine") rather than ("mine" and "mine") or ("theirs" and "theirs")
+     * @param  array $answers Array containing agents' answers to a certain question.
+     * @return boolean        True if the answers tally, false if not.
+     */
+    private function answersTally($answers) {
+        switch($answers['question_id']) {
             case 'article_11':
-                return $this->answersMatch($result);
+                return $this->answersMatch($answers);
             case 'article_1':
-                return $this->answerIsOneOf($result, array(
+                return $this->answerIsOneOf($answers, array(
                     array('mine', 'other'),
                     array('both', 'both')
                 ));
             case 'article_2':
-                return $this->answersMatch($result);
+                return $this->answersMatch($answers);
             case 'article_3':
-                return $this->answerIsOneOf($result, array(
+                return $this->answerIsOneOf($answers, array(
                     array('mine', 'other'),
                     array('both', 'both')
                 ));
             case 'article_7a':
-                return $this->answersMatch($result);
+                return $this->answersMatch($answers);
             case 'article_7b':
-                return $this->answersMatch($result);
+                return $this->answersMatch($answers);
             default:
                 return false;
         }
     }
 
+    /**
+     * Are both agents' answers the same?
+     * @param  array $answers Array containing both agents' answers.
+     * @return boolean        True if answers match, otherwise false.
+     */
+    private function answersMatch($answers) {
+        return $answers['your_answer'] === $answers['their_answer'];
+    }
+
+    /**
+     * Checks if the agents' answers match one of the possible answer sets.
+     * @param  array $answer          Agents' answers
+     * @param  array $possibleAnswers Possible answer sets.
+     * @return boolean                True if answer is one of the possible answer sets, otherwise false.
+     */
+    private function answerIsOneOf($answers, $possibleAnswers) {
+        foreach($possibleAnswers as $possibleAnswer) {
+            if ($this->answerIs($answers, $possibleAnswer)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Compares agents' answers with an answer set, returning true if there is a match.
+     * @param  array $result  Agents' answers.
+     * @param  array $answer  Possible answer set.
+     * @return boolean        True if the answers match the answer set, otherwise false.
+     */
+    private function answerIs($result, $answer) {
+        return (
+            ($result['your_answer'] === $answer[0] && $result['their_answer'] === $answer[1]) ||
+            ($result['your_answer'] === $answer[1] && $result['their_answer'] === $answer[0])
+        );
+    }
+
+    /**
+     * Deduces the summary for the maritime collision, based on the agents' answers.
+     * @param  array $results Array of questions, answers and whether or not they tally.
+     * @return array<string>  Array of summaries. Some summaries are complicated and split into multiple elements in an array, which should be mapped to new paragraphs. Simple summaries will only be an array of one element.
+     */
     private function deduceSummary($results) {
         $summary = array();
         $complexSummary = false;
@@ -202,6 +251,11 @@ class ResultsCalculator {
         return $summary;
     }
 
+    /**
+     * In cases where the collision was not accidental, the calculation of awarded damages is quite complicated. This is encapsulated in its own function, and directly alters the $summary parameter.
+     * @param  array         $results    Array of questions, answers and whether or not they tally.
+     * @param  array<string> &$summary  Array of summaries.
+     */
     private function deduceDamages($results, &$summary) {
         $vesselResponsible = $this->getResultsForQuestion('article_3', $results)['your_answer'];
 
